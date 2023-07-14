d3657c335eca9e27abebbc4efd8cbb344de60751
2bona <sayhitobona@gmail.com>
2023-07-14 12:28:21 +0100

    dd

---
+++ components/vendorsettings.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-container fluid="">
+        <p style="color:#333;
+font-size: 24px;
+font-weight: 500;">Manage your account settings</p>
+<v-sheet class="cardStyle">
+    v-
+
+</v-sheet>
+    </v-container>
+</template>
