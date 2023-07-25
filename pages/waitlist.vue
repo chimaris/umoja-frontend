a02cc341a1d49535d453da46a4c04cf8b0588bec
2bona <sayhitobona@gmail.com>
2023-07-25 21:05:56 +0100

    dd

---
+++ pages/waitlist.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">
+          S/N
+        </th>
+        <th class="text-left">
+          Name
+        </th>
+        <th class="text-left">
+          Email
+        </th>
+        <th class="text-left">
+          Category
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(item, i) in list"
+        :key="i"
+      >
+        <td>{{ i + 1 }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.email }}</td>
+        <td>{{ item.category }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+        dialog: false,
+        mods:1,
+        name: '',
+        dialog2: false,
+        loading: false,
+        category:null,
+        email: '',
+        emailtxt: '',
+        list: [],
+        rules: {
+        required: (value) => !!value || "Required.",
+        email: (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        },
+      },
+    };
+  },
+  mounted() {
+    axios.get('https://umoja-eaac4dc3d4d4.herokuapp.com/api/get_waitlist').then(response => {
+        this.list = response.data.list
+    })
+},
+  methods: {
+    opendialog(){
+        this.dialog = true
+    },
+    submitForm() {
+    this.loading = true;
+      axios.post('https://umoja-eaac4dc3d4d4.herokuapp.com/api/join_waitlist', {
+        name: this.name,
+        category: this.category,
+        email: this.email
+      })
+      .then(response => {
+        this.loading = false;
+        this.emailtxt = this.email
+        this.name = ''
+        this.email = ''
+        this.category = null
+        this.dialog = false
+        this.dialog2 = true
+        
+        console.log(response.data);
+        // Handle successful response here
+      })
+      .catch(error => {
+      this.dialog = false
+          this.loading = false;
+        console.error(error);
+        alert('An error occurred')
+        // Handle error here
+      });
+    }
+  }
+};
+</script>
