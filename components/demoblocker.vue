4ab24c23b057178edcd3d322f3413ca8d94041e2
2bona <sayhitobona@gmail.com>
2023-10-25 00:19:03 -0500

    dd

--- components/demoblocker.vue
+++ components/demoblocker.vue
@@ -1,7 +1,7 @@
 <template>
     
 <div class="w-100 " style="z-index: 9999; position: fixed;top: 0;bottom: 0;left: 0;width: 100%;height: 100vh;" >
-<v-card width="100%" height="" class="px-4 text-center" color="red" style="position: fixed;bottom: 0;left: 0;">
+<v-card flat rounded="0" width="100%" height="" class="px-4 text-center" color="red" style="position: fixed;bottom: 0;left: 0;">
  <h1 class="py-12">
    Open in desktop to view the demo
 
