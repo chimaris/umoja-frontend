a846cc79fee4fedb74707c2d4d86c531bb16906c
2bona <sayhitobona@gmail.com>
2023-10-25 23:18:18 -0500

    dd

--- components/demoblocker.vue
+++ components/demoblocker.vue
@@ -3,7 +3,7 @@
 <div class="w-100 " style="z-index: 9999; position: fixed;top: 0;bottom: 0;left: 0;width: 100%;height: 100vh;" >
 <v-card flat rounded="0" width="100%" height="" class="px-4 text-center" color="red" style="position: fixed;bottom: 0;left: 0;">
  <h1 class="py-12">
-   Open in desktop to view the demo
+    Mobile version coming soon, Open in desktop to view the demo
 
  </h1>
 </v-card>
