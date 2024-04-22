7ac2541cf395d572c434de664dcead8d0c8633ea
Stellamaris Nwafor <wendumaris@gmail.com>
2024-04-22 18:15:23 +0100

    responsiveness on the home page

--- components/vendorComponent.vue
+++ components/vendorComponent.vue
@@ -1,5 +1,11 @@
 <template>
-	<v-card :width="$vuetify.display.mobile ? 184 : 254" v-if="loaded" style="overflow: visible" flat class="bg-white parent-card rounded-lg mb-2">
+	<v-card
+		:style="{ width: $vuetify.display.mobile ? '100%' : '254px' }"
+		v-if="loaded"
+		style="overflow: visible"
+		flat
+		class="bg-white parent-card rounded-lg mb-2"
+	>
 		<v-img eager class="bg-grey-lighten-5" cover style="border-radius: 16px" width="100%" height="237" :src="item.image"> </v-img>
 		<div class="px-4">
 			<v-avatar style="margin-top: -37px" :size="$vuetify.display.mobile ? 43 : 67">
@@ -14,16 +20,29 @@
 			</p>
 			<div class="d-block mt-1">
 				<div class="d-flex align-center">
-					<p style="color: #969696; font-size: 14px; font-weight: 500; line-height: 140%" class="text-capitalize">{{ category }}</p>
+					<p
+						:style="{ fontSize: $vuetify.display.mobile ? '10px' : '14px' }"
+						style="color: #969696; font-weight: 500; line-height: 140%"
+						class="text-capitalize"
+					>
+						{{ category }}
+					</p>
 					<v-icon class="mx-1" size="5" color="grey-lighten-1" icon="mdi mdi-circle"></v-icon>
-					<p style="color: #969696; font-size: 14px; font-weight: 500; line-height: 140%">{{ item.subCategory }}</p>
+					<p :style="{ fontSize: $vuetify.display.mobile ? '10px' : '14px' }" style="color: #969696; font-weight: 500; line-height: 140%">
+						{{ item.subCategory }}
+					</p>
 				</div>
 			</div>
 			<p style="font-weight: 600; font-size: 10px; line-height: 10px; color: #000000" class="d-flex mb-1 pb-0 pt-1 align-center">
 				<v-rating readonly model-value="4" color="grey-lighten-2" active-color="#E7CE5D" class="rts" density="compact" size="x-small"></v-rating
 				><span style="margin-left: 3px; margin-top: 3px">(101)</span>
 			</p>
-			<div variant="text" size="x-small" style="font-size: 12px; font-weight: 500; border-radius: 6px; color: #f38218; letter-spacing: -0.12px">
+			<div
+				variant="text"
+				size="x-small"
+				:style="{ fontSize: $vuetify.display.mobile ? '10px' : '12px' }"
+				style="font-weight: 500; border-radius: 6px; color: #f38218; letter-spacing: -0.12px"
+			>
 				<v-icon size="14" class="mr-2" style="transform: rotate(90deg)" icon="mdi mdi-tag"></v-icon>Delivery within 5 Working Days
 			</div>
 		</div>
