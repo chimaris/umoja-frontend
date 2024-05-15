2e66496f8a3b62843ee8d3f779dd7c55a9da60e7
Stellamaris Nwafor <wendumaris@gmail.com>
2024-05-15 09:44:54 +0100

    updates

--- pages/signin-category.vue
+++ pages/signin-category.vue
@@ -5,7 +5,7 @@
 		<div style="min-height: 80vh" class="d-flex align-center justify-center pa-3">
 			<div>
 				<p
-					:style="{ fontSize: $vuetify.display.mobile ? '24px' : '48px' }"
+					:style="{ fontSize: $vuetify.display.mobile ? '28px' : '48px' }"
 					style="font-weight: 700; line-height: 60px; text-align: center; letter-spacing: -0.03em"
 					class="my-12 text-center"
 				>
@@ -15,18 +15,18 @@
 					flat
 					@click="$router.push('/user/login')"
 					:style="{ width: $vuetify.display.mobile ? '100%' : '859px', height: $vuetify.display.mobile ? 'auto' : '169px' }"
-					class="mb-8 py-4 rounded-lg pa-6 d-flex align-center"
+					class="mb-8 py-4 rounded-lg pa-2 pa-md-6 d-flex align-center"
 				>
 					<div class="d-flex align-center" style="margin-right: 100px">
-						<v-avatar :size="$vuetify.display.mobile ? 40 : 80" class="mr-6" variant="tonal" color="green">
+						<v-avatar :size="$vuetify.display.mobile ? 40 : 80" class="mr-2 mr-md-6" variant="tonal" color="green">
 							<v-icon :size="$vuetify.display.mobile ? 20 : 40" class="" icon="mdi mdi-account"></v-icon>
 						</v-avatar>
 
-						<div>
-							<p :style="{ fontSize: $vuetify.display.mobile ? '14px' : '24px' }" style="font-weight: 700; line-height: 30px; color: #333333">
+						<div class="mr-2 mr-md-0">
+							<p :style="{ fontSize: $vuetify.display.mobile ? '16px' : '24px' }" style="font-weight: 700; line-height: 30px; color: #333333">
 								Sign in as a Customer
 							</p>
-							<p :style="{ fontSize: $vuetify.display.mobile ? '8px' : '16px' }" style="font-weight: 400; max-width: 317px; color: #333333">
+							<p :style="{ fontSize: $vuetify.display.mobile ? '10px' : '16px' }" style="font-weight: 400; max-width: 317px; color: #333333">
 								Lorem ipsum dolor sit amet consectetur. Sit urna neque pellentesque at cras.
 							</p>
 						</div>
@@ -38,17 +38,17 @@
 					:style="{ width: $vuetify.display.mobile ? '100%' : '859px', height: $vuetify.display.mobile ? 'auto' : '169px' }"
 					flat
 					@click="$router.push('/vendor/login')"
-					class="mb-4 rounded-lg pa-6 d-flex align-center"
+					class="mb-4 rounded-lg pa-2 pa-md-6 d-flex align-center"
 				>
 					<div class="d-flex align-center" style="margin-right: 100px">
-						<v-avatar :size="$vuetify.display.mobile ? 40 : 80" class="mr-6" variant="tonal" color="green">
+						<v-avatar :size="$vuetify.display.mobile ? 40 : 80" class="mr-2 mr-md-6" variant="tonal" color="green">
 							<v-icon :size="$vuetify.display.mobile ? 20 : 40" class="" icon="mdi mdi-store"></v-icon>
 						</v-avatar>
-						<div>
-							<p :style="{ fontSize: $vuetify.display.mobile ? '14px' : '24px' }" style="font-weight: 700; line-height: 30px; color: #333333">
+						<div class="mr-2 mr-md-0">
+							<p :style="{ fontSize: $vuetify.display.mobile ? '16px' : '24px' }" style="font-weight: 700; line-height: 30px; color: #333333">
 								Sign in as a Vendor
 							</p>
-							<p :style="{ fontSize: $vuetify.display.mobile ? '8px' : '16px' }" style="font-weight: 400; color: #333333; max-width: 317px">
+							<p :style="{ fontSize: $vuetify.display.mobile ? '10px' : '16px' }" style="font-weight: 400; color: #333333; max-width: 317px">
 								Lorem ipsum dolor sit amet consectetur. Sit urna neque pellentesque at cras.
 							</p>
 						</div>
