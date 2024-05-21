0a1974c1175f5471e5243da41fbf44bb8690f04f
IwuchukwuDivine <ekeneifunanya@gmail.com>
2024-05-21 15:33:22 +0100

    fix: like post

--- plugins/fetch-product.js
+++ plugins/fetch-product.js
@@ -1,10 +1,12 @@
 import { useProductStore } from '~/stores/productStore';
 import { fetchAllPosts, fetchAllArticle } from '~/composables/usePost';
+import { useUserStore } from "~/stores/userStore";
 
 
 export default defineNuxtPlugin(async (nuxtApp) => {
+  const userStore = useUserStore();
   const productStore = useProductStore();
   await productStore.fetchProducts();
-  fetchAllPosts()
-  fetchAllArticle()
+  fetchAllPosts(userStore.postPage)
+  fetchAllArticle(userStore.articlePage)
 });
