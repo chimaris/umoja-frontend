1024fd7c746fae8f84fc76864d0c36f721e39cba
IwuchukwuDivine <ekeneifunanya@gmail.com>
2024-06-04 16:35:53 +0100

    .

--- components/vendordashboard.vue
+++ components/vendordashboard.vue
@@ -1,63 +1,44 @@
 <template>
-	<v-container style="max-width: 1330px" class="w-100 mt-5 pt-0 pr-8 pl-5">
-		<div class="pb-5 d-flex align-center justify-space-between">
-			<p style="font-weight: 600; font-size: 24px; line-height: 30px">Hi {{ vendor.first_name }} {{ vendor.last_name }}, Welcome back</p>
+    <v-container style="max-width: 1330px;" class="w-100 mt-5 pt-0 pr-8 pl-5">
 
  <div class="pb-5 d-flex align-center justify-space-between">
   <p style="font-weight: 600; text-transform: capitalize;
 font-size: 24px;
 line-height: 30px;">Hi {{vendor.first_name}} {{ vendor.last_name }}, Welcome back </p>
 
-				<v-text-field
-					style=""
-					variant="outlined"
-					density="compact"
-					class="ml-2 borderedinput"
-					hide-details=""
-					prepend-inner-icon="mdi mdi-magnify"
-					placeholder="Search"
-				></v-text-field>
-			</div>
-		</div>
-		<div class="d-flex justify-center">
-			<v-row style="overflow-x: scroll; -webkit-overflow-scrolling: touch; display: grid; grid-auto-flow: column">
-				<v-col cols="12" lg="3" v-for="n in dashes">
-					<v-card min-width="300px" class="mx-auto cardStyle" width="100%" style="" flat>
-						<div class="d-flex align-center">
-							<v-avatar class="mr-2" size="34"><v-img :src="n.img"></v-img></v-avatar>
-							<p
-								style="
-									font-weight: 500;
-									font-size: 20px;
-									line-height: 25px;
+  <div class="d-flex align-center">
+    <v-menu width="200">
+      <template v-slot:activator="{ props }">
+      
+        <div v-bind="props" v-ripple  style="  border-radius: 6px;  border: 1px solid #e5e5e5; width:200px;
+" variant="outlined" class="px-4 py-2 d-flex align-center justify-space-between text-grey-darken-3" >
+<span>
 
-									color: #333333;
-								"
-							>
-								{{ n.name }}
-							</p>
-							<div class="ml-2">
-								<v-icon size="16" icon="mdi mdi-information"> </v-icon>
-								<v-tooltip activator="parent" location="end" class="text-red">{{ n.tooltip }}</v-tooltip>
-							</div>
-						</div>
-						<div class="d-flex align-center mt-4 justify-space-between">
-							<h1 class="bigpriceClass2">{{ n.amount }}</h1>
-							<div class="text-left">
-								<p
-									style="font-weight: 600; font-size: 14px; line-height: 18px; color: #00966d"
-									class="text-grey-lighten-2"
-									:class="n.trending ? 'text-grey-lighten-2' : 'text-red'"
-								>
-									<v-icon :icon="n.trending ? 'mdi mdi-trending-up' : 'mdi mdi-trending-down'"></v-icon> {{ n.trend }}%
-								</p>
-								<p style="font-weight: 500; font-size: 14px; line-height: 18px; letter-spacing: -0.01em; color: #969696">vs last 7 days</p>
-							</div>
-						</div>
-					</v-card>
-				</v-col>
-			</v-row>
-		</div>
+  <v-icon class="mr-2" icon="mdi mdi-tune-vertical-variant"></v-icon> Filter 
+</span>
+<v-icon class="" icon="mdi mdi-chevron-down"></v-icon>
+</div>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in ['Last 7 days', 'This Month', 'This Year', 'Custom']"
+          :key="index"
+          :value="index"
+        >
+          <v-list-item-title>{{ item }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  
+    <v-text-field style="" variant="outlined" density="compact" class="ml-2 borderedinput" hide-details="" prepend-inner-icon="mdi mdi-magnify" placeholder="Search"></v-text-field>
+  </div>
+ </div>
+      <div class="d-flex justify-center">
+<v-row style=" overflow-x: scroll;
+  -webkit-overflow-scrolling: touch;
+  display: grid;
+  grid-auto-flow: column;">
+<v-col cols="12" lg="3" v-for="n in dashes">
 
     <v-card min-width="300px" class="mx-auto cardStyle" width="100%" style=""  flat >
         <div class="d-flex  align-center">
@@ -261,90 +242,6 @@ color: #969696;" class="ml-4">{{ n?.percentage }}%</p>
         line-height: 100%;
         /* identical to box height, or 10px */
 
-							<!-- </v-img> -->
-						</v-col>
-						<v-col v-if="hasReview" class="" cols="5">
-							<div class="mt-3" v-for="n in countries" :key="n.name">
-								<div class="d-flex mb-1 align-center">
-									<v-avatar size="24"><v-img :src="n.img"></v-img></v-avatar>
-									<p style="font-weight: 500; font-size: 16px" class="ml-3 text-capitalize">{{ n.name }}</p>
-								</div>
-								<div class="d-flex align-center">
-									<div style="width: 80%">
-										<v-progress-linear
-											style="width: 100%; border-radius: 5px"
-											class="mr-2"
-											color="#1361F4"
-											:model-value="n.growth"
-											:height="6"
-										></v-progress-linear>
-									</div>
-									<p style="font-weight: 500; font-size: 16px; line-height: 20px; color: #969696" class="ml-4">{{ n.growth }}%</p>
-								</div>
-							</div>
-						</v-col>
-					</v-row>
-				</v-card>
-			</v-col>
-			<v-col cols="12" lg="5">
-				<v-card height="100%" class="mx-auto coolTable py-4" width="100%" style="overflow: hidden" flat>
-					<div class="d-flex px-4 justify-space-between">
-						<div>
-							<p class="mb-2 timernum d-flex align-center text-left">Top Transactions</p>
-							<p class="lightText2">of the week based on total purchase</p>
-						</div>
-						<v-btn v-if="hasSale" variant="text" color="#1361F4" class="">View All</v-btn>
-					</div>
-					<div
-						v-if="!hasSale"
-						style="
-							display: flex;
-							flex-direction: column;
-							justify-content: center;
-							color: #969696;
-							width: 100%;
-							height: 100%;
-							gap: 10px;
-							align-items: center;
-						"
-						class=""
-					>
-						<v-icon><i class="mdi mdi-block-helper"></i></v-icon>
-						<span>No Product has been sold yet</span>
-					</div>
-					<div class="rounded- mt-5">
-						<v-table v-if="hasSale" style="height: 190px !important; overflow: scroll">
-							<thead>
-								<tr class="bg-grey-lighten-3">
-									<th style="font-size: 14px" class="font-weight-medium text-left">#ID</th>
-									<th style="font-size: 14px" class="text-left px-1 font-weight-medium">First Item</th>
-									<th style="font-size: 14px" class="text-left px-1 font-weight-medium">Date</th>
-									<th style="font-size: 14px" class="text-left px-1 font-weight-medium">Purchase</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr @click="chosen = item.sn" :style="chosen == item.sn ? 'background:#DFDFDF' : ''" v-for="item in items" :key="item.sn">
-									<td style="font-size: 14px" class="px-1 pl-2">
-										<v-menu location="end">
-											<template v-slot:activator="{ props }">
-												<span v-bind="props">
-													{{ item.sn }}
-												</span>
-											</template>
-											<v-card class="pa-4 rounded-lg bg-green">
-												<p><v-icon class="mr-2" size="6" icon="mdi mdi-circle"></v-icon>Customer ID: {{ item.sn }}</p>
-												<div class="pt-2 align-center d-flex">
-													<v-avatar size="44" class="mr-2"
-														><v-img src="https://res.cloudinary.com/payhospi/image/upload/v1685693849/Rectangle_1898_gyahsj.png"></v-img
-													></v-avatar>
-													<div>
-														<p style="font-weight: 500; font-size: 14px" class="text-capitalize">Jenny Wilson</p>
-														<p
-															style="
-																font-weight: 500;
-																font-size: 10px;
-																line-height: 100%;
-																/* identical to box height, or 10px */
 
         color: rgba(255, 255, 255, 0.75);" class="text-capitalize">
         <v-icon size="12" class="mr-1" icon="mdi mdi-map-marker"></v-icon>{{item.user_country}}</p>
@@ -585,19 +482,23 @@ View Details            </v-btn>
 
 </template>
 <style>
-.reviewstab .v-carousel .v-btn--icon .v-icon {
-	font-size: 9px;
+
+.reviewstab .v-carousel .v-btn--icon .v-icon{
+font-size: 9px;
 }
 .reviewstab .v-carousel--hide-delimiter-background .v-carousel__controls {
-	bottom: -33px;
+    bottom: -33px;
 }
-.reviewstab .v-carousel .v-btn--icon.v-btn--active .v-icon {
-	color: orange;
+.reviewstab .v-carousel .v-btn--icon.v-btn--active .v-icon{
+color: orange;
 }
-.reviewstab .rev .v-rating__item .v-btn {
-	scale: 2 !important;
-	margin-left: -13px !important;
+.reviewstab .rev .v-rating__item .v-btn{
+  scale: 2!important;
+    margin-left: -13px!important;
+
+
 }
+
 </style>
 <script setup>
 import { useVendorStore } from '~/stores/vendorStore';
